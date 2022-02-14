7e7d48e0545e1c9195ce8f8953622bd3a651adf5
chongliujia <jcl314159@gmail.com>
2022-02-14 15:27:51 +0800

    add AlexNet for MNIST Datasets

--- model/model.py
+++ model/model.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
-
+import torch
 
 class MnistModel(BaseModel):
     def __init__(self, num_classes=10):
@@ -20,3 +20,51 @@ class MnistModel(BaseModel):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
+class AlexNet(BaseModel):
+    def __init__(self, num_classes=10):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.relu1 = nn.ReLU(inplace=True)
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.relu2 = nn.ReLU(inplace=True)
+
+        self.conv3 = nn.Conv2d( 64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.relu3 = nn.ReLU(inplace=True)
+
+        self.fc6 = nn.Linear(256 * 3 * 3, 1024)
+        self.fc7 = nn.Linear(1024, 512)
+        self.fc8 = nn.Linear(512, 10)
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.relu1(x)
+
+        x = self.conv2(x)
+        x = self.pool2(x)
+        x = self.relu2(x)
+
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.pool3(x)
+        x = self.relu3(x)
+
+        x = x.view(-1, 256 * 3 * 3)
+        x = self.fc6(x)
+        x = F.relu(x)
+        x = self.fc7(x)
+        x = F.relu(x)
+        x = self.fc8(x)
+
+        return x
+                
